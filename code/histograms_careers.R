1f95ed719e1263487f59e4e78b0e6f22c2f3ba05
catherinelunny <lunnyc@bc.edu>
2023-11-16 11:19:19 -0500

    player seasons played

--- code/histograms_careers.R
+++ code/histograms_careers.R
@@ -13,3 +13,17 @@ p <- ggplot(tab_games_by_players,
 print(p)
 
 ggsave(filename = "results/hist_games_played.png",plot = p, bg = "white")
+
+
+tab_seasons_by_players <- all_plays %>%
+  group_by(player_id) %>%
+  summarise(unique_seasons = length(unique(season)))
+
+t <- ggplot(tab_seasons_by_players, 
+            aes(x = unique_seasons)) +
+  geom_histogram(bins = 100) +
+  theme_minimal() +
+  labs(x="# of unique seasons", y= "# of players")
+print(p)
+
+ggsave(filename = "results/hist_seasons_played.png",plot = t, bg = "white")
